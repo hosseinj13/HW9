b65398e94a47f1dd3fd6fe63747261384b980c70
hossein javadi <hosseinj13@yahoo.com>
2024-02-11 17:25:16 +0330

    create User.java and add AllArgsConstructor , NoArgsConstructor, Getter, Setter , ToString with lombok.

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -11,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ToString
 
+
 public class Category extends BaseEntity<Integer> {
 
     String categoryName;
