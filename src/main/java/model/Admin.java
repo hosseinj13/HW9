9346f2f99dd8082aaaa0089ef2d4b771e5e7d601
hossein javadi <hosseinj13@yahoo.com>
2024-02-11 16:34:01 +0330

    create Admin.java

---
+++ src/main/java/model/Admin.java
@@ -0,0 +1,27 @@
+package model;
+
+import base.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+
+public class Admin extends BaseEntity<Integer> {
+    String name;
+    String username;
+    String email;
+    String password;
+
+    public Admin(Integer adminId, String name, String username, String email, String password) {
+        super(adminId);
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
